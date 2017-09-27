ce0a1622402744278c49c525163fdc646f62f0fd
jankidepala <jankidepala@gmail.com>
2017-09-27 23:47:32 +0530

    Router Angular4.0

--- Angular4.StarterProject/src/app/app.module.ts
+++ Angular4.StarterProject/src/app/app.module.ts
@@ -1,12 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { CurrencyFormaterDirective } from './directives/currency-formater.directive';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { PatientComponent } from './components/patient/patient.component';
+
+const appRoutes: Routes = [
+  {path: '', component: LoginComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'dashboard', component: DashboardComponent},
+  {path: 'patient', component: PatientComponent},
+];
 
 @NgModule({
   declarations: [
@@ -15,10 +23,12 @@ import { FooterComponent } from './components/footer/footer.component';
     DashboardComponent,
     CurrencyFormaterDirective,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    PatientComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
